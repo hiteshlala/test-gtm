386f4bd36a15cbde1c952f91289c5b9791dac302
hiteshlala <hiteshlala@gmail.com>
2021-09-18 05:24:19 -0700

    investigate cookie

--- docs/index.js
+++ docs/index.js
@@ -10,3 +10,26 @@ function sendEvent(e, v) {
     dataLayer.push(event);
   }
 }
+
+function toggleConsent() {
+  const cookies = document.cookie.split(';')
+  .filter(f => !!f)
+  .map(v => {
+    const [ key, value ] = v.split('=').map(a=> a.trim());
+    return { key, value };
+  });
+  if ( cookies.length ) {
+    for( let i = 0; i < cookies.length; i++) {
+      const c = cookies[i];
+      if( c.key === 'consent') {
+        c.value === 'true' ? c.value = 'false' : c.value = 'true';
+      }
+    }
+  }
+  else {
+    cookies.push({key: 'consent', value: 'false' });
+  }
+  const toSet = cookies.reduce((res, cookie) => `${res}${cookie.key}=${cookie.value};samesite;`,'');
+  document.cookie = toSet;
+  console.log(toSet)
+}
